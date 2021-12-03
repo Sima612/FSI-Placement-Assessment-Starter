ccbb401935652940a1ea33f5606dba650c2e1cd4
Sima Tran <simatran@Simas-MacBook-Pro.local>
2021-12-02 21:25:51 -0800

    Completed assignment

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,62 @@
-// HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Rachasima Tran"
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
-
+let total = 0
 // Code to update name display 
-document.getElementById('credit').textContent = `Created by ${yourName}`
+document.getElementById('credit').textContent = `Created by Rachasima Tran`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
     console.log('Gingerbread + button was clicked!')
-
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+    gb++;
+    total++;
+document.getElementById('qty-gb').textContent = gb;
+document.getElementById('qty-total').textContent = total;
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+document.getElementById('minus-gb').addEventListener('click', function() {
+    console.log('Gingerbread - button was clicked!')
+    if(gb > 0) {
+    gb--;
+    total--;
+    }
+document.getElementById('qty-gb').textContent = gb;
+document.getElementById('qty-total').textContent = total;
+})
+
+document.getElementById('add-cc').addEventListener('click', function() {
+    console.log('Chocolate Chip + button was clicked!')
+    cc++;
+    total++;
+document.getElementById('qty-cc').textContent = cc;
+document.getElementById('qty-total').textContent = total;
+})
+
+document.getElementById('minus-cc').addEventListener('click', function() {
+    console.log('Chocolate Chip - button was clicked!')
+    cc--;
+    total--;
+document.getElementById('qty-cc').textContent = cc;
+document.getElementById('qty-total').textContent = total;
+})
+
+document.getElementById('add-sugar').addEventListener('click', function() {
+    console.log('Sugar Sprinkle + button was clicked!')
+    sugar++;
+    total++;
+document.getElementById('qty-sugar').textContent = sugar;
+document.getElementById('qty-total').textContent = total;
+})
+
+document.getElementById('minus-sugar').addEventListener('click', function() {
+    console.log('Sugar Sprinkle - button was clicked!')
+    sugar--;
+    total--;
+document.getElementById('qty-sugar').textContent = sugar;
+document.getElementById('qty-total').textContent = total;
+})
